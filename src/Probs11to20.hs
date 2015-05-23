6773f89141f8d88917e75f3d223ea33e169eb780
Aj <btchaggler@gmail.com>
2015-05-23 12:20:12 +0000

    problems from 11 to 20 are finished

--- src/Probs11to20.hs
+++ src/Probs11to20.hs
@@ -47,39 +47,43 @@ dropnth l n = map fst
             $ filter (not . (==n) . snd)
             $ zip l (mconcat $ repeat [1..n])
 
-type Splitlength = Int
-splitlist :: [a] -> Splitlength -> ([a], [a])
+type Index = Int
+splitlist :: [a] -> Index -> ([a], [a])
+--Given a list and an Index, split the list at the
+--Index.
 splitlist l n = (take n l, drop n l)
 
-type Index = Int
 mslice :: [a] -> Index -> Index -> Maybe [a]
+--Given 2 indices, return the portion of the list
+--in between those indices
 mslice l s e
     | (null l) || (s > e) = Nothing
     | otherwise           = (Just . take (e-s+1) . drop (s-1)) l
 
 mrotate :: [a] -> Int -> [a]
+--Rotate the list left and right
+--A positive value rotates right and a negative
+--value rotates left
 mrotate l n
     | n > 0 = uncurry (flip (++)) (splitlist l n)
     | otherwise = uncurry (flip (++)) (splitlist l ((length l) + n))
 
 
-dropkth :: [a] -> Int -> (a, [a])
+dropkth :: [a] -> Int -> (Maybe a, [a])
+--Return a tuple containing the dropped element
+--and the entire list without the dropped element
+--Inefficient
 dropkth [] _ = (Nothing, [])
-dropkth l n = head $ drop (n-1) l
+dropkth l n = (Just $ head $ drop (n-1) l, (take (n-1) l) ++ (drop n l))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+mdropkth :: [a] -> Int -> (Maybe a, [a])
+--Return a tuple containing the dropped element
+--and the entire list without the dropped element
+--More efficient than the previous version. Explicit
+--recursion
+mdropkth l n = fn l n []
+  where
+    fn [] _ ys = (Nothing, ys)
+    fn (x:xs) 1 ys = (Just x, (reverse ys) ++ xs)
+    fn (x:xs) i ys = fn xs (i-1) (x:ys)
 
